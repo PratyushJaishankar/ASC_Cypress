ed7f5024dfcd6381499321ae98c316c6ac549b85
Pratyush Jaishankar <pratjai97@gmail.com>
2025-09-18 00:08:05 +0530

    Added mocha_reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     experimentalStudio: true,
-      defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 10000,
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: false,
+      json: true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('task', {
+      on("task", {
         log(message) {
           console.log(message);
           return null;
